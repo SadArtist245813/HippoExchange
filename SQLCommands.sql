42190d073a7a402ebc4cc908cdf7bdb9ab58cef1
Justin Hortopanu <72450220+SadArtist245813@users.noreply.github.com>
2025-12-02 15:56:46 -0600

    1.0 Save/Transfer
    Add mySql Commands

---
+++ SQLCommands.sql
@@ -0,0 +1,44 @@
+SHOW TABLES;
+
+CREATE TABLE tblUsers(
+	Email VARCHAR(250) PRIMARY KEY,
+	FirstName VARCHAR(25),
+    LastName VARCHAR(25),
+    Password VARCHAR(500),
+    CreatedDateTime datetime,
+    LastUsedDateTime datetime
+);
+
+CREATE TABLE tblSessions(
+	SessionID VARCHAR(50) PRIMARY KEY,
+    UserID VARCHAR(250),
+    CreatedDateTime datetime
+);
+
+CREATE TABLE tblBrands(
+	BrandName VARCHAR(250) PRIMARY KEY,
+    CreatedDateTime datetime
+);
+
+CREATE TABLE tblInventory(
+	InventoryID VARCHAR(50) PRIMARY KEY,
+    Brand VARCHAR(250),
+    Model VARCHAR(250),
+    Description VARCHAR(2000),
+    Owner VARCHAR(250),
+    Active boolean
+);
+
+CREATE TABLE tblLoans(
+	LoanID VARCHAR(50) PRIMARY KEY,
+    InventoryID VARCHAR(50),
+    Borrower VARCHAR(250),
+    DateOfLoan datetime,
+    DateOfReturn datetime
+);
+
+DROP TABLE tblUsers;
+DROP TABLE tblSessions;
+DROP TABLE tblBrands;
+DROP TABLE tblInventory;
+DROP TABLE tblLoans;
